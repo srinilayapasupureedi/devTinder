049bb21e85fd51583f6281f0003a17ff98c0d17c
Srinilaya Pasupureddi <srinilayap@gmail.com>
2025-10-03 11:57:27 +0530

    schemaValidations

--- src/models/user.js
+++ src/models/user.js
@@ -2,25 +2,53 @@ const mongoose = require('mongoose');
 const userSchema=new mongoose.Schema({
     firstName:{
       type:String,
+      required:true,
+      minlength:3,
+      maxlength:30,
       
     },
     lastName:{
       type:String,
-    
+      required:true,
+      minlength:3,
+      maxlength:30,
+
     },
     email:{
+      unique:true,
       type:String,
-      
+      required:true,
+      lowercase:true,
+      minlength:5,
+      maxlength:100,
+      validate(value){
+        const emailRegex=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if(!emailRegex.test(value)){
+          throw new Error("Invalid email");
+        }
+      }
     },
     password:{
       type:String,
+      required:true,
     },
     age:{
       type:Number,
-      
+      min:18,
+
     },
     gender:{
       type:String,
+      required:true,
+      validate(value){
+        if(!["male","female","other"].includes(value)){
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    about:{
+      type:String,
+      default:"this is the default about section",
     }
-});
+},{timestamps:true});
 module.exports=mongoose.model("User",userSchema);
